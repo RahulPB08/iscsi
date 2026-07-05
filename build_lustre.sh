d38e34d491071b805906147956774c6d6f229fc3
Rahul Bhosle <150593765+RahulPB08@users.noreply.github.com>
2026-07-05 08:47:18 +0000

    dfcvdffgv

--- build_lustre.sh
+++ build_lustre.sh
@@ -4,12 +4,12 @@
 # Install and configure Lustre servers (MGS,MDS,OSS) common packages
 
 # Get RPM packages
-wget $(cat to_downloads_servers)
-# Install Lustre server kernel
-yum -y localinstall kernel-3.10.0-693.5.2.el7_lustre.x86_64.rpm 
-# Setup lustre linux-modified kernel and reboot
-/sbin/new-kernel-pkg --package kernel --mkinitrd --dracut --depmod --install 3.10.0-693.5.2.el7_lustre.x86_64
-shutdown -r now
+# wget $(cat to_downloads_servers)
+# # Install Lustre server kernel
+# yum -y localinstall kernel-3.10.0-693.5.2.el7_lustre.x86_64.rpm 
+# # Setup lustre linux-modified kernel and reboot
+# /sbin/new-kernel-pkg --package kernel --mkinitrd --dracut --depmod --install 3.10.0-693.5.2.el7_lustre.x86_64
+# shutdown -r now
 # Install `e2fsprogs` (Provides utilities for working with ext2/ext3/ext4 file systems)
 yum -y localinstall e2fsprogs-1.42.13.wc6-7.el7.x86_64.rpm  e2fsprogs-libs-1.42.13.wc6-7.el7.x86_64.rpm \
 libcom_err-1.42.13.wc6-7.el7.x86_64.rpm libss-1.42.13.wc6-7.el7.x86_64.rpm
